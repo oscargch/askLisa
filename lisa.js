0fb7ecc0dfefc8c9f4cd3803031e0a46fd1ba422
Oscar Garcia <oscar.gch@gmail.com>
2020-09-24 00:32:07 -0500

    changed the functionality a bit

--- lisa.js
+++ lisa.js
@@ -1,6 +1,6 @@
 // initial HTML
 let coreHTML = `<textarea class="textinput" name="thingy" wrap="on" rows="100" cols="50"></textarea>`;
-let bodyDiv = document.querySelector('.boody');
+let bodyDiv = document.querySelector(".boody");
 
 let textHTML = document.createRange().createContextualFragment(coreHTML);
 // console.log(coreHTML);
@@ -9,63 +9,62 @@ document.body.appendChild(textHTML);
 
 // After initial HTML dump variables:
 
-let textArea = document.querySelector('.textinput');
+let textArea = document.querySelector(".textinput");
 let askString = `Lisa, could you please answer my question? `; // 42 characters
 let welcome = `I'm Lisa. Ask nicely or face the consequences. `;
 
 // logic:
 function writeFirst() {
-printLetterByLetter(textArea, welcome, 25);
+  printLetterByLetter(textArea, welcome, 25);
 }
 
 writeFirst();
 
-function printLetterByLetter(destination, message, speed){
-var i = 0;
-var interval = setInterval(function(){
-destination.innerHTML += message.charAt(i);
-i++;
-if (i > message.length){
-clearInterval(interval);
-}
-}, speed);
+function printLetterByLetter(destination, message, speed) {
+  var i = 0;
+  var interval = setInterval(function() {
+    destination.innerHTML += message.charAt(i);
+    i++;
+    if (i > message.length) {
+      clearInterval(interval);
+    }
+  }, speed);
 }
 
 //la respuesta debe ser escrita entre dos characteres identificables.
 
 //this is an option, but maybe they should be mapped. each character is bound to the other, that way if they are deleted they both get deleted.
-const permitedKeys = ['[', ']', 'Enter', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '.', ',', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', '?'];
 
-var regex = /(?<=\[)(.*?)(?=\])/;
-let whatsWritten = '';
+let regex = /(?<=\[)(.*?)(?=\])/;
+let whatsWritten = "";
 let whatsWrittenCount = 0;
 
-window.addEventListener('keydown', pressedKey);
+document.body.addEventListener("keydown", pressedKey);
 
 function pressedKey(e) {
-if (e.key == 'Shift') {
-whatsWrittenCount = whatsWrittenCount;
-} else if (permitedKeys.includes(e.key)) {
-whatsWritten = whatsWritten.concat(e.key);
-whatsWrittenCount ++;
-  replaceFirst42(textArea, askString, 25);
-} else if (e.key == 'Backspace') {
-whatsWritten = whatsWritten.slice(0, -1);
-whatsWrittenCount --;
-// replaceFirst42(textArea, e.key, 25);
-} 
-if (whatsWrittenCount < 0){
-  whatsWrittenCount = 0;
+  if (e.key == "Shift") {
+    whatsWrittenCount = whatsWrittenCount;
+  } else if (permitedKeys.includes(e.key)) {
+    whatsWritten = whatsWritten.concat(e.key);
+    whatsWrittenCount++;
+    replaceFirst42(textArea, askString, 25);
+  } else if (e.key == "Backspace") {
+    whatsWritten = whatsWritten.slice(0, -1);
+    whatsWrittenCount--;
+    // replaceFirst42(textArea, e.key, 25);
   }
-if (whatsWrittenCount < 43){
-  e.preventDefault();
+  if (whatsWrittenCount < 0) {
+    whatsWrittenCount = 0;
   }
-if (e.key == 'Enter') {
-  getAnswer(whatsWritten);
+  if (whatsWrittenCount < 43) {
+    e.preventDefault();
   }
-console.log(`${whatsWrittenCount} ${whatsWritten}`);
-moveCursorToEnd(textArea);
-} 
+  if (e.key == "Enter") {
+    getAnswer(whatsWritten);
+  }
+  console.log(`${whatsWrittenCount} ${whatsWritten}`);
+  moveCursorToEnd(textArea);
+}
 
 letterCount = 0;
 // var caretPosition = document.caretPositionFromPoint(float x, float y);
@@ -82,29 +81,47 @@ function replaceFirst42(destination, message) {
 //move cursor to end (el) is a reference to an input or textarea
 function moveCursorToEnd(el) {
   if (typeof el.selectionStart == "number") {
-      el.selectionStart = el.selectionEnd = el.value.length;
+    el.selectionStart = el.selectionEnd = el.value.length;
   } else if (typeof el.createTextRange != "undefined") {
-      el.focus();
-      var range = el.createTextRange();
-      range.collapse(false);
-      range.select();
+    el.focus();
+    var range = el.createTextRange();
+    range.collapse(false);
+    range.select();
   }
 }
 
 function getAnswer(whatsWritten) {
-  let newtextArea = document.querySelector('.answer');
-  console.log('running getAnswer');
+  let newtextArea = textArea; //change it to ".answer" when done
+  // console.log("running getAnswer");
+  // console.log(newtextArea);
+  // console.log(newtextArea.value);
   let hiddenAnswer = regex.exec(whatsWritten);
-  console.log(`secret answer: ${hiddenAnswer[0]}`);
-  console.log(`secret answer: ${typeof(hiddenAnswer[0])}`);
-  console.log(`new text area: ${textArea}`);
-  revealSecret();
-          function revealSecret() {
-            printAnswerByLetter(newtextArea, hiddenAnswer[0]);
-          }
+  // let answerInHTML = document.createRange().createContextualFragment(hiddenAnswer);
+  // console.log(`Answer in HTML: ${answerInHTML}`);
+  // console.log(`secret answer: ${answerInHTML}`);
+  // console.log(`secret answer: ${typeof answerInHTML}`);
+  // console.log(`new text area: ${newtextArea}`);
+  revealSecret(newtextArea, hiddenAnswer);
+  function revealSecret(newtextArea, hiddenAnswer) {
+    printAnswerByLetter(newtextArea, hiddenAnswer);
+  }
 }
 
-function printAnswerByLetter(destination, secretAnswer) {
-  console.log('running printAnswerByLetter');
-  destination.insertAdjacentHTML('beforeend', secretAnswer);
-  }
+function printAnswerByLetter(destination, hiddenAnswer) {
+  let priorvalue = destination.value;
+  console.log("running printAnswerByLetter");
+  console.log(destination);
+  destination.value = `${priorvalue} ${hiddenAnswer[0]}`;
+  // whatsWritten = "";
+  // whatsWrittenCount = 0;
+  // letterCount = 0;
+}
+
+// En la segunda vuelta esta intentando usar printLetterByLetter() pero eso no va a functionar de nuevo.
+// La funcion printLetterByLetter() deberria funcionar de una manera distinta... de adherir algo al valor en la ultima parte.
+
+// textContent =
+// innerText =
+// innerHTML =; // replace HTML
+// insertAdjacentHTML("beforeend", secretAnswer); // insert at some point
+// appendChild(answerInHTML); // added to (used before with contextualfragment)
